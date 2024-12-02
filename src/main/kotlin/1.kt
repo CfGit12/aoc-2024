d8036d9b2aabd49ba3be3e3ad6bae9d6118bd953
Charles Flynn <charles.flynn@fanduel.com>
2024-12-02 08:22:15 +0000

    Use class to drive execution and parsing

--- src/main/kotlin/1.kt
+++ src/main/kotlin/1.kt
@@ -1,22 +1,27 @@
 import kotlin.math.abs
 
-private val input = readFileAsLines("1.txt")
-    .map { it.split("   ")  }
+private typealias ListPair = Pair<List<Int>, List<Int>>
 
-fun main() {
-    println(part1())
-    println(part2())
-}
+object Day1 : AocDay<ListPair>(1) {
 
-private fun part1(): Int {
-    val lhs = input.map { it[0].toInt() }.sorted()
-    val rhs = input.map { it[1].toInt() }.sorted()
-    return lhs.zip(rhs).sumOf { abs( it.second - it.first) }
-}
+    override fun part1(input: ListPair): Any {
+        val lhs = input.first.sorted()
+        val rhs = input.second.sorted()
+        return lhs.zip(rhs).sumOf { abs(it.second - it.first) }
+    }
 
-private fun part2(): Int {
-    val lhs = input.map { it[0].toInt() }
-    val rhs = input.map { it[1].toInt() }
+    override fun part2(input: ListPair): Any {
+        val lhs = input.first
+        val rhs = input.second
 
-    return lhs.fold(0) { acc, i -> acc + (i * rhs.count { it == i }) }
+        return lhs.fold(0) { acc, i -> acc + (i * rhs.count { it == i }) }
+    }
+
+    override fun parse(inputStr: String): ListPair {
+        val lines = inputStr.lines().map { it.split("   ") }
+        val lhs = lines.map { it[0].toInt() }
+        val rhs = lines.map { it[1].toInt() }
+        return lhs to rhs
+    }
+
 }
---
+++ src/main/kotlin/AocDay.kt
@@ -0,0 +1,17 @@
+abstract class AocDay<T>(private val day: Int) {
+
+    private val input: T = parse(readFile("$day.txt"))
+    private val sampleInput: T = parse(readFile("$day-sample.txt"))
+
+    abstract fun part1(input: T): Any
+    abstract fun part2(input: T): Any
+    abstract fun parse(inputStr: String): T
+
+    fun execute() {
+        println("--- DAY $day ---")
+        println("Part 1: ${part1(input)}")
+        println("Part 2: ${part2(input)}")
+        println("Part 1 Sample: ${part1(sampleInput)}")
+        println("Part 2 Sample: ${part2(sampleInput)}")
+    }
+}
